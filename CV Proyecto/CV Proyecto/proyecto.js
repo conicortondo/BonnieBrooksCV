03ad473289df0b3c52393ef1661a32edca54e6b9
Constanza Cortondo <stancoz_04@hotmail.com>
2023-02-26 14:10:33 -0300

    cv bonnie brooks

---
+++ CV Proyecto/CV Proyecto/proyecto.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').addEventListener('click',function() {
+    console.log ("Hola mundo desde EventListener");
+    document.getElementById('demo').innerHTML = "Soy una programadora web con más de 5 años de experiencia contribuyendo desde el Front End al desarrollo de aplicaciones web y UI para diferentes sectores. Mi interes principal gira en torno a la UX, por ese motivo me enfoque en diseño de interiores."
+}); 
